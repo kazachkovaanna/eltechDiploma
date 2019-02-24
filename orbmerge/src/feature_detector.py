5ec1397e7c6e2531039c15f96f2ca8b6277c0ca6
kazachkovaanna <kazachkova.ann@gmail.com>
2019-02-24 15:07:09 -0500

    image merging

--- orbmerge/src/feature_detector.py
+++ orbmerge/src/feature_detector.py
@@ -1,29 +1,80 @@
+from __future__ import print_function
 import numpy as np
+from shutil import copyfile
+
 import cv2
-from matplotlib import pyplot as plt
-img1 = cv2.imread('/home/user/ros/dip/logger/catkin_ws/bags/bag061827/map0019.pgm',0)          # queryImage
-img2 = cv2.imread('/home/user/ros/dip/logger/catkin_ws/bags/bag062926/map0021.pgm',0) # trainImage
-# Initiate SIFT detector
-# sift = cv2.SIFT()
-sift = cv2.xfeatures2d.SIFT_create()
-# find the keypoints and descriptors with SIFT
-kp1, des1 = sift.detectAndCompute(img1,None)
-kp2, des2 = sift.detectAndCompute(img2,None)
-# FLANN parameters
-FLANN_INDEX_KDTREE = 1
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks=50)   # or pass empty dictionary
-flann = cv2.FlannBasedMatcher(index_params,search_params)
-matches = flann.knnMatch(des1,des2,k=2)
-# Need to draw only good matches, so create a mask
-matchesMask = [[0,0] for i in xrange(len(matches))]
-# ratio test as per Lowe's paper
-for i,(m,n) in enumerate(matches):
-    if m.distance < 0.7*n.distance:
-        matchesMask[i]=[1,0]
-draw_params = dict(matchColor = (0,255,0),
-                   singlePointColor = (255,0,0),
-                   matchesMask = matchesMask,
-                   flags = 0)
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
-plt.imshow(img3,),plt.show()
+import  os
+
+MAX_FEATURES = 500
+GOOD_MATCH_PERCENT = 0.1
+
+
+def alignImages(im1, im2, path):
+    # Convert images to grayscale
+    im1Gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
+    im2Gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+
+    # Detect ORB features and compute descriptors.
+    orb = cv2.ORB_create(MAX_FEATURES)
+    keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
+    keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
+
+    # Match features.
+    matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
+    matches = matcher.match(descriptors1, descriptors2, None)
+
+    # Sort matches by score
+    matches.sort(key=lambda x: x.distance, reverse=False)
+
+    # Remove not so good matches
+    numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
+    matches = matches[:numGoodMatches]
+
+    # Draw top matches
+    imMatches = cv2.drawMatches(im1, keypoints1, im2, keypoints2, matches, None)
+    cv2.imwrite(path + "matches.jpg", imMatches)
+
+    # Extract location of good matches
+    points1 = np.zeros((len(matches), 2), dtype=np.float32)
+    points2 = np.zeros((len(matches), 2), dtype=np.float32)
+
+    for i, match in enumerate(matches):
+        points1[i, :] = keypoints1[match.queryIdx].pt
+        points2[i, :] = keypoints2[match.trainIdx].pt
+
+    # Find homography
+    h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+
+    # Use homography
+    height, width, channels = im2.shape
+    im1Reg = cv2.warpPerspective(im1, h, (width, height))
+
+    return im1Reg, h
+
+
+if __name__ == '__main__':
+    dirBagOne = '/home/user/ros/dip/logger/catkin_ws/bags/bag061827/'
+    dirBagTwo = '/home/user/ros/dip/logger/catkin_ws/bags/bag062926/'
+    os.makedirs('vers 0.2')
+
+    for bagOneImage in os.listdir(dirBagOne):
+        print("Reading reference image : ", bagOneImage)
+        imReference = cv2.imread(dirBagOne + bagOneImage, cv2.IMREAD_COLOR)
+        for bagTwoImage in os.listdir(dirBagTwo):
+            print("Reading image to align : ", bagTwoImage)
+            im = cv2.imread(dirBagTwo + bagTwoImage, cv2.IMREAD_COLOR)
+            print("Aligning images ...")
+            # Registered image will be resotred in imReg.
+            # The estimated homography will be stored in h.
+            try:
+                os.makedirs('vers 0.2/' + bagOneImage + "-" + bagTwoImage)
+                imReg, h = alignImages(im, imReference, 'vers 0.2/' + bagOneImage + "-" + bagTwoImage + '/')
+                outFilename = 'vers 0.2/' + bagOneImage + "-" + bagTwoImage + '/' + bagOneImage + "-" + bagTwoImage + ".jpg"
+                print("Saving aligned image : ", outFilename)
+                copyfile(dirBagOne + bagOneImage, 'vers 0.2/' + bagOneImage + "-" + bagTwoImage + '/' + bagOneImage)
+                copyfile(dirBagTwo + bagTwoImage, 'vers 0.2/' + bagOneImage + "-" + bagTwoImage + '/' + bagTwoImage)
+                cv2.imwrite(outFilename, imReg)
+                # Print estimated homography
+                print("Estimated homography : \n", h)
+            except cv2.error as e:
+                print ("Error aligning " + bagOneImage + " with " + bagTwoImage)
